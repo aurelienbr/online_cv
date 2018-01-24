5f7ab4d2dcd191e8c55184a3fc9dc9df94b6db3d
aurelienbr <aurelien.brachet@viacesi.fr>
2018-01-24 14:13:25 +0100

    adding local translations

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,46 @@
 import axios from 'axios';
 const URL = 'http://localhost:3000/translations';
 
-export const getTranslations = () => dispatch => {
-  dispatch({
-    type: 'GETTING_TRANSLATION',
-  });
-  axios.get(URL).then(response => {
-    dispatch({
-      payload: response.data,
+const TRANSLATIONS = {
+  en: {
+      "header.home": "home",
+      "header.about": "about me",
+      "header.education": "education",
+      "header.contact": "contact",
+      "home.name": "Aurelien Brachet",
+      "home.fonction": "Javascript Developper",
+      "about.title": "About me",
+      "education.education": "Education",
+      "education.internship": "Internship",
+      },
+      fr: {
+        "header.home": "acceuil",
+      "header.about": "a propos",
+      "header.education": "parcours",
+      "header.contact": "contact",
+      "home.name": "Aurelien Brachet",
+      "home.fonction": "Développeur Javascript",
+      "about.title": "A propos",
+      "education.education": "Scolarite",
+      "education.internship": "Stage",
+      },
+      ru:{
+        "header.home": "прием",
+      "header.about": "кстати",
+      "header.education": "образование",
+      "header.contact": "контакт",
+      "home.name": "Орельен Браше",
+      "home.fonction": "Программист JavaScript",
+      "about.title": "Обо мне",
+      "education.education": "Oбразование",
+      "education.internship": "Интернатура"
+    }
+}
+
+export const getTranslations = ()  => ({
+      payload: TRANSLATIONS,
       type: 'GET_TRANSLATIONS',
-    });
-  });
-};
+});
 
 export const changeLocale = (locale) => ({
 	payload: locale,
